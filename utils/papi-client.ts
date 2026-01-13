eeab1368f032d940947ee691a6d928ac15e7820d
HOP <mjga2025ardoino@gmail.com>
2026-01-13 09:47:47 +0100

    Delete utils directory

--- utils/papi-client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@polkadot-api/client";
-import { chainSpec } from "@polkadot-api/descriptors"; // Generato dopo il setup
-import { getScProvider } from "@polkadot-api/sc-provider";
-
-// Utilizziamo un nodo RPC pubblico per Paseo Asset Hub
-const scProvider = getScProvider();
-const provider = scProvider.addChain(chainSpec);
-
-export const client = createClient(provider);
