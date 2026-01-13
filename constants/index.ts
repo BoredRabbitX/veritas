0bd5a457ea9c57aeb46b0ddcd1145824db65d184
HOP <mjga2025ardoino@gmail.com>
2026-01-13 09:46:06 +0100

    Delete constants directory

--- constants/index.ts
+++
@@ -1,8 +0,0 @@
-export const CONTRACT_ADDRESS = "0xb3dc45518cc51c759193da59772a407dad584852";
-
-export const CONTRACT_ABI = [
-  "function registerBusiness(string _name, string _category) external",
-  "function postReview(address _business, uint8 _rating, string _ipfsContent, bytes32 _receiptId, bytes _signature) external",
-  "function getAverageRating(address _business) external view returns (uint256)",
-  "function businesses(address) external view returns (string name, string category, bool isActive, uint256 reviewCount)"
-];
